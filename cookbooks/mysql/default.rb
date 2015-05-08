738f04c762b923b0a47324a459bba02e1b4c3176
sambaiz <godgourd@gmail.com>
2015-05-08 18:27:25 +0900

    >< wip

--- cookbooks/mysql/default.rb
+++ cookbooks/mysql/default.rb
@@ -4,7 +4,7 @@ execute "mysql_secure_installation" do
   user "root"
   only_if "mysql -u root -e 'show databases' | grep information_schema" # パスワードが空の場合
   command <<-EOL
-    mysqladmin -u root password "your_password"
+    mysqladmin -u root password "password"
     mysql -u root -pyour_password -e "DELETE FROM mysql.user WHERE User='';"
     mysql -u root -pyour_password -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');"
     mysql -u root -pyour_password -e "DROP DATABASE test;"
@@ -12,6 +12,8 @@ execute "mysql_secure_installation" do
     mysql -u root -pyour_password -e "FLUSH PRIVILEGES;"
   EOL
 end
+
+remote_file "/etc/my.cnf"
 
 service 'mariadb' do
   action [:start, :enable]
--- cookbooks/redmine/default.rb
+++ cookbooks/redmine/default.rb
@@ -3,3 +3,21 @@ execute "svn co http://svn.redmine.org/redmine/branches/3.0-stable /var/lib/redm
 end
 
 remote_file "/var/lib/redmine/config/database.yml"
+remote_file "/etc/httpd/conf.d/redmine.conf"
+
+execute "sudo sed -i 's/^Defaults\\s*secure_path\\s*=.*/Defaults secure_path = \\/sbin:\\/bin:\\/usr\\/sbin:\\/usr\\/bin:\\/usr\\/local\\/bin/' /etc/sudoers"
+
+%w(mariadb-server mariadb-devel httpd-devel ruby ruby-devel openssl-devel
+readline-devel zlib-devel curl-devel libyaml-devel libffi-devel libxml2-devel
+libxslt-devel ImageMagick ImageMagick-devel ipa-pgothic-fonts).each do |p|
+    package p
+end
+
+execute "migration" do
+    command <<-EOL
+        cd /var/lib/redmine
+        gem install bundler
+        bundle install
+        RAILS_ENV=production rake db:migrate
+    EOL
+end
