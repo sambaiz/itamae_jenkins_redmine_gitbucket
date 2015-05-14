89c0d26b405055b152a864b5c0ad66ac548bf6c3
sambaiz <godgourd@gmail.com>
2015-05-14 16:51:38 +0900

    install git and gitbucket plugin to jenkins

--- cookbooks/jenkins/default.rb
+++ cookbooks/jenkins/default.rb
@@ -12,3 +12,14 @@ remote_file "/etc/httpd/conf.d/jenkins.conf"
         action [:restart]
     end
 end
+
+# gitとgitbucketのプラグインを入れる
+# 再起動を待ってからjenkins-cli.jarを取得する
+execute "get jenkins-cli.jar" do
+    command <<-EOL
+        sleep 30
+        wget http://localhost/jenkins/jnlpJars/jenkins-cli.jar -P /tmp
+    EOL
+    not_if "test -e /tmp/jenkins-cli.jar"
+end
+execute "java -jar /tmp/jenkins-cli.jar -s http://localhost/jenkins/ install-plugin git gitbucket -deploy"
